e979f14e73dab71c4f4d98f1ff1efb3ee87450d2
Ty Salter <saltj002@mymail.unisa.edu.au>
2020-08-21 12:13:17 +0930

    Implement constructor and displayWelcome function

--- core/menuinterface.cpp
+++ core/menuinterface.cpp
@@ -3,9 +3,13 @@
 using namespace core;
 
 
-MenuInterface::MenuInterface()
-{
+MenuInterface::MenuInterface(std::ostream &display, std::istream &input): _display{display}, _input{input} {
 
 }
 
+void MenuInterface::displayWelcome(const std::string author, const std::string title) const{
+    _display << "Welcome to: " << title << std::endl;
+    _display << "\t\tDeveloped by " << author << std::endl;
+    _display << "COMP 3023 Software Development with C++" << std::endl;
+}
 
--- core/menuinterface.h
+++ core/menuinterface.h
@@ -1,13 +1,21 @@
 #ifndef MENUINTERFACE_H
 #define MENUINTERFACE_H
 
+#include <iostream>
+
 namespace core {
 
 
     class MenuInterface
     {
     public:
-        MenuInterface();
+        MenuInterface(std::ostream &display, std::istream &input);
+        void displayWelcome(const std::string author, const std::string title) const;
+        void run();
+
+    private:
+        std::ostream &_display;
+        std::istream &_input;
     };
 }
 
